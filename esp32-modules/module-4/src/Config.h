67e082306dd14cfcf2fb7abe0832a70c768ae7de
VladimirManev <145099305+VladimirManev@users.noreply.github.com>
2026-01-14 09:08:22 +0100

    Change table auto duration to 20 seconds

--- esp32-modules/module-4/src/Config.h
+++ esp32-modules/module-4/src/Config.h
@@ -52,7 +52,7 @@
 
 // Table timing settings
 #define TABLE_DOUBLE_CLICK_TIMEOUT 500  // ms - max time between clicks for double-click detection
-#define TABLE_AUTO_MOVE_DURATION 5000   // ms - duration for auto movement on double-click
+#define TABLE_AUTO_MOVE_DURATION 20000  // ms - duration for auto movement on double-click
 #define TABLE_START_DELAY 600           // ms - delay before starting motor (longer than double-click timeout)
 
 // Debug settings
